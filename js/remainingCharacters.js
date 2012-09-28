d99f35247a0354dc4514fb6e8ec0bb4de7438476
Bryan Johns <bryan.johns@mh.alabama.gov>
2012-09-28 11:58:46 -0500

    Added a new configuration property called targetStyle to allow the user to apply css styling to the <div> that is generated at runtime if one is not supplied. This feature gives a great deal of flexibility in as it would allow just about any style imaginable to the target div.
    Also upgraded to jquery 1.8.2 and everything
    still works.
    
    modified:   css/site.css
    modified:   index.html
    deleted:    js/jquery-1.7.1.min.js
    new file:   js/jquery-1.8.2.min.js
    new file:   js/remainingCharacters.js
    deleted:    js/remainingCharacters.min.js

---
+++ js/remainingCharacters.js
@@ -0,0 +1,82 @@
+/*
+* remainingCharacters JavaScript Library v2.3.0
+* http://github.com/k4gdw/jQuery.remainingCharacters
+*
+* Copyright 2012 K4GDW Software. All rights reserved.
+*
+* Redistribution and use in source and binary forms,   with or without
+* modification, are permitted provided that the  following  conditions
+* are met:
+* 
+* 1.	Redistributions of source code must retain the above copyright
+*		notice, this list of conditions and the  following disclaimer.
+* 
+* 2.	Redistributions  in  binary  form  must  reproduce  the  above
+*		copyright notice, this list of conditions  and  the  following
+*		disclaimer in the   documentation   and/or   other   materials
+*		provided with the distribution.
+* 
+* THIS SOFTWARE  IS PROVIDED BY K4GDW SOFTWARE "AS IS" AND ANY EXPRESS
+* OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED  TO,  THE  IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR  A  PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL K4GDW SOFTWARE OR CONTRIBUTORS  BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR
+* CONSEQUENTIAL  DAMAGES  (INCLUDING, BUT NOT  LIMITED TO, PROCUREMENT
+* OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;  OR
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+* OR OTHERWISE) ARISING IN ANY WAY  OUT OF THE USE   OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+(function ($) {
+    $.fn.remainingCharacters = function (args) {
+		var targetWidth = $(this).outerWidth();
+		var targetStyle = args.targetStyle || 'width: ' + targetWidth + 'px; text-align: right;';
+    	var target = args.target || $('<div>', {
+    			id: 'rcOutput',
+    			style: targetStyle
+    		}).insertBefore(this);
+    	var maxChars = args.maxChars;
+    	var hideTarget = args.hideTarget || false;
+    	var current = this.val() || '';
+    	var fadeTarget;
+    	var fadeSpeed;
+    	if(args.fadeTarget){
+    		fadeTarget = args.fadeTarget.fade || false;
+    		fadeSpeed = args.fadeTarget.speed || 'fast';
+    	}
+    	if (hideTarget){
+    		$(target).hide();	
+    	}
+    	$(target).html((maxChars - current.length) + ' characters remaining.');
+        this.keyup(function() {
+            $(target).html((maxChars - this.value.length) + ' characters remaining.');
+        }).keydown(function(e) {
+        	if (this.value.length > (maxChars - 1)) {
+				if (((e.keyCode >= 48) && (e.keyCode <= 111))
+						|| ((e.keyCode >= 186) && (e.keyCode <= 222)) 
+							|| e.keyCode == 32
+								|| ((e.keyCode == 13) && (this.tagName == 'TEXTAREA'))) {
+						return false;
+					}
+				}
+				return true;
+        	}).focus(function(){        		
+        		if (fadeTarget){
+        			$(target).fadeIn(fadeSpeed);
+        			$(target).html((maxChars - this.value.length) + ' characters remaining.');
+	        	} else {
+	        		$(target).show();
+	        	}
+        	}).blur(function(){
+        		if (hideTarget){
+        			if (fadeTarget){
+        				$(target).fadeOut(fadeSpeed);
+	        		} else {
+	        			$(target).hide();
+	        		}
+        		}
+        	});	
+        return this;
+	};
+})( jQuery );
